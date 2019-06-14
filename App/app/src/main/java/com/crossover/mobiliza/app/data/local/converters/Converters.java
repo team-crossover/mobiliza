9331dba123c98559c8c6db6fa82cffe82a0df0a9
NelsonWilliam <nelsonwilliam@live.com>
2019-06-14 09:42:18 -0300

    Alteração na conversão de Calendar

--- App/app/src/main/java/com/crossover/mobiliza/app/data/local/converters/Converters.java
+++ App/app/src/main/java/com/crossover/mobiliza/app/data/local/converters/Converters.java
@@ -6,26 +6,40 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Converters {
-    @TypeConverter
-    public static Calendar fromTimestamp(Long value) {
-        if (value == null)
-            return null;
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(value);
-        return calendar;
-    }
 
-    @TypeConverter
-    public static Long dateToTimestamp(Calendar date) {
-        return date == null ? null : date.getTimeInMillis();
-    }
+    private static SimpleDateFormat dateTimeFormat
+            = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US);
 
     private static Gson gson = new Gson();
+
+    @TypeConverter
+    public static Calendar stringToCalendar(String value) {
+        if (value == null)
+            return null;
+        try {
+            Date date = dateTimeFormat.parse(value);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            return calendar;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @TypeConverter
+    public static String calendarToString(Calendar date) {
+        return date == null ? null : dateTimeFormat.format(date);
+    }
 
     @TypeConverter
     public static List<Long> stringToLongList(String data) {
