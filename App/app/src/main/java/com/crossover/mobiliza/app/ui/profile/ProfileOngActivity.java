1a7c601662cd0fdc9c35c2933579a2a113392892
João Pedro Vieira <jp-a.v@hotmail.com>
2019-06-19 12:22:36 -0300

    Adição de negrito em TextView

--- App/app/src/main/java/com/crossover/mobiliza/app/ui/profile/ProfileOngActivity.java
+++ App/app/src/main/java/com/crossover/mobiliza/app/ui/profile/ProfileOngActivity.java
@@ -8,7 +8,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
